59e0bdedd6b99c00d7a49fa2aa507c612faf179c
unfunkyufo <ghidorah_69@yahoo.com>
2018-01-28 16:51:17 +0800

    defined path

--- src/gameselection.c
+++ src/gameselection.c
@@ -3,12 +3,14 @@
 #include <dirent.h>
 #include <stdio.h>
 
+#define GAMES_PATH "./games/"
+
 void chooseAGame() {
 	//scan user input for game title
 	//execute the chosen program
 	DIR *d;
 	struct dirent *dir;
-	d = opendir(".");
+	d = opendir(GAMES_PATH);
 	if (d) {
 		while ((dir = readdir(d)) != NULL) {
 			printf("%s\n", dir->d_name);
